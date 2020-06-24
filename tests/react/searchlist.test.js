26357788bc82cb1b25ebcca420fe6c52d4d2e4f4
Alex Broaddus <abro@google.com>
2020-06-24 18:10:36 -0400

    Adding tests for searchlist component

---
+++ tests/react/searchlist.test.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {render, fireEvent, waitFor, screen} from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import SearchList from '../../src/components/searchlist.js';
+
+/**
+ * Returns an array of items labeled as so: item1, item2...
+ * @param {number} numItems Number of items to generate
+ * @return {string[]} List of example items
+ */
+const generateItems = (numItems) => {
+  const items = [];
+  for (let i = 1; i <= numItems; i++) {
+    items.push('item' + i);
+  }
+  return items;
+};
+
+test('Items do not display until data is done loading', () => {
+  const items = generateItems(10);
+  const {rerender} = render(
+      <SearchList items={items} onClickItem={() => {}} isLoading={true}/>,
+  );
+
+  // Ensure no items are displayed
+  expect(() => {
+    screen.getAllByText('item', {exact: false});
+  }).toThrow();
+
+  // Set isLoading=false and check items again
+  rerender(
+      <SearchList items={items} onClickItem={() => {}} isLoading={false}/>,
+  );
+  expect(screen.getAllByText('item', {exact: false})).toHaveLength(10);
+});
+
+test('Only display first 50 items until user scrolls', () => {
+  const items = generateItems(70);
+  render(
+      <SearchList items={items} onClickItem={() => {}} isLoading={false}/>,
+  );
+
+  expect(screen.getAllByText('item', {exact: false})).toHaveLength(50);
+
+  // Scrolling should increase list size by 10
+  fireEvent.scroll(document, {target: {scrollY: 100}});
+  expect(screen.getAllByText('item', {exact: false})).toHaveLength(60);
+
+  // Scroll again to ensure all items are displayed
+  fireEvent.scroll(document, {target: {scrollY: 100}});
+  expect(screen.getAllByText('item', {exact: false})).toHaveLength(70);
+});
