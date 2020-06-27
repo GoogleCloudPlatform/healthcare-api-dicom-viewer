ef573094f6ea817ec61070a603f0c14fd49ba162
Alex Broaddus <abro@google.com>
2020-06-26 21:10:14 -0400

    Fixed timeouts on tests to avoid occasional failures

--- tests/react/searchlist.test.js
+++ tests/react/searchlist.test.js
@@ -74,21 +74,21 @@ test('Typing in search bar filters items', async () => {
   // Searching for item10 should filter to only one item
   fireEvent.input(screen.getByRole('textbox'), {target: {value: 'item10'}});
   await waitFor(() =>
-    expect(screen.getAllByText('item', {exact: false})).toHaveLength(1));
+    expect(screen.getAllByText('item', {exact: false})).toHaveLength(1), {timeout: 2000});
 
   // Searching for item6 should filter to two items (item6 and item60)
   fireEvent.input(screen.getByRole('textbox'), {target: {value: 'item6'}});
   await waitFor(() =>
-    expect(screen.getAllByText('item', {exact: false})).toHaveLength(2));
+    expect(screen.getAllByText('item', {exact: false})).toHaveLength(2), {timeout: 2000});
 
   // Searching for blank space should return all items (only 50 will be
   // displayed as that is the initial max number of items until scroll)
   fireEvent.input(screen.getByRole('textbox'), {target: {value: ' '}});
   await waitFor(() =>
-    expect(screen.getAllByText('item', {exact: false})).toHaveLength(50));
+    expect(screen.getAllByText('item', {exact: false})).toHaveLength(50), {timeout: 2000});
 
   // Searching for item70 should yield no items
   fireEvent.input(screen.getByRole('textbox'), {target: {value: 'item70'}});
   await waitFor(() =>
-    expect(screen.queryAllByText('item', {exact: false})).toHaveLength(0));
-});
+    expect(screen.queryAllByText('item', {exact: false})).toHaveLength(0), {timeout: 2000});
+}, 20000);
