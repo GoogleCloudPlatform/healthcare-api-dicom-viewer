d860de97081783b0afa716c41c26adecd200ea81
Alex Broaddus <abro@google.com>
2020-07-07 12:25:17 -0400

    Removed cornerstone-wado-image-loader

--- src/cornerstonesetup.js
+++ src/cornerstonesetup.js
@@ -1,20 +1,4 @@
 import * as cornerstone from 'cornerstone-core';
-import * as cornerstoneWADOImageLoader from 'cornerstone-wado-image-loader';
-import * as dicomParser from 'dicom-parser';
-import * as auth from './auth.js';
 import * as dicomImageLoader from './dicomImageLoader.js';
 
 cornerstone.registerImageLoader('dicomImageLoader', dicomImageLoader.loadImage);
-cornerstoneWADOImageLoader.external.cornerstone = cornerstone;
-cornerstoneWADOImageLoader.external.dicomParser = dicomParser;
-cornerstoneWADOImageLoader.configure({
-  beforeSend: (xhr) => {
-    const accessToken = auth.getAccessToken();
-    if (accessToken) {
-      xhr.setRequestHeader('Accept', 'application/dicom; transfer-syntax=*');
-      xhr.setRequestHeader('Authorization', `Bearer ${accessToken}`);
-    } else {
-      auth.signInToGoogle();
-    }
-  },
-});
