c022b911be538637d778f6d376c3b6579d2135b7
Alex Broaddus <alexbroaddus1@gmail.com>
2020-08-21 11:22:58 -0400

    Removed client id

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@
 
 /** Client ID for this application in Google Cloud
  * @constant {string} */
-const CLIENT_ID = '485533938322-72mcbc75qib3sg3ms71otq4poebelnio.apps.googleusercontent.com'; /** Fill this in with your own client-id */
+const CLIENT_ID = 'YOUR_CLIENT_ID'; /** Fill this in with your own client-id */
 
 /** Prefix to use on instance url's to inform cornerstoneJS to use our image loader
  * @constant {string} */
