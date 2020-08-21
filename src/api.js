d852917088f0702f0291a738a0209b867143c84c
Alex Broaddus <alexbroaddus1@gmail.com>
2020-08-20 23:43:27 -0400

    Removed a resolved TODO

--- src/api.js
+++ src/api.js
@@ -200,7 +200,6 @@ const fetchMetadata =
  * @return {Uint8Array} Byte array of DICOM P10 contents
  */
 const fetchDicomFile = async (url) => {
-  // TODO(#10) Revisit using gzip without multipart headers once fix is launched
   // TODO(#11) Investigate optimal accept header for compressed instances
   const response = await authenticatedFetch(url, {
     headers: {
